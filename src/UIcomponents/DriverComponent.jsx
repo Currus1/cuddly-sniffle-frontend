06458a65bf133f16378f30257672d84782acff19
Klaidas Sinkevicius <clyde.sinkevicius@gmail.com>
2022-10-06 17:32:32 +0300

    KS/700

--- src/UIcomponents/DriverComponent.jsx
+++ src/UIcomponents/DriverComponent.jsx
@@ -2,80 +2,119 @@ import React, { useState, useEffect } from "react";
 import "../bootstrap.css";
 import "../App.css";
 import "./styles/DriverStyle.css";
-import DriverAPI from "../DriverServices/DriverAPI";
-import { Grid, Paper, Avatar, Typography, TextField, Button } from '@material-ui/core'
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
-
+import {
+  Grid,
+  Paper,
+  Avatar,
+  Typography,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import TripAPI from "../TripServices/TripAPI";
 
 const DriverComponent = () => {
-  const [drivers, setDriver] = useState([]);
+  const [vehicleType, setVehicleType] = useState("");
+  const [vehicleTypes, setVehicleTypes] = useState([]);
 
   useEffect(() => {
-    loadTable();
-  }, []); // initializing the table on load (once)
+    TripAPI.getVehicleTypeEnum().then((response) =>
+      setVehicleTypes(response.data)
+    );
+    setVehicleType("Sedan"); // Initial value for select
+  }, []);
 
-  function loadTable() {
-    DriverAPI.GetAllDrivers()
-      .then((res) => setDriver(res.data))
-      .catch((err) => console.log(err));
-  }
+  const handleVehicleTypeChange = (event) => {
+    setVehicleType(event.target.value);
+  };
 
-  function SaveClicked() {
-    if (
-      document.getElementById("Id").value !== "" &&
-      document.getElementById("Name").value !== "" &&
-      document.getElementById("Surname").value !== "" &&
-      document.getElementById("Birthdate").value !== "" &&
-      document.getElementById("Email").value !== "" &&
-      document.getElementById("PhoneNumber").value !== ""
-    ) {
-      drivers.Id = document.getElementById("Id").value;
-      drivers.Name = document.getElementById("Name").value;
-      drivers.Surname = document.getElementById("Surname").value;
-      drivers.Birthdate = document.getElementById("Birthdate").value;
-      drivers.Email = document.getElementById("Email").value;
-      drivers.PhoneNumber = document.getElementById("PhoneNumber").value;
-      drivers.VehicleType = document.getElementById("VehicleType").value;
-      drivers.LicenseNumber = document.getElementById("LicenseNumber").value;
-
-      DriverAPI.AddingDriver(drivers);
-      console.log("Registered");
-    } else {
-      alert("Please make sure to enter all the fields");
-    }
-  }
-
-  // Load the drivers to data const via useState
-  function LoadClicked() {
-    loadTable();
-  }
-
-  const avatarStyle = {backgroundColor: '#0099CC'}
-  const marginTop = {marginTop: 15}
+  const avatarStyle = { backgroundColor: "#0099CC" };
+  const marginTop = { marginTop: 15 };
+  const bigMarginTop = { marginTop: 30 };
+  const buttonStyle = {
+    marginTop: 30,
+    backgroundColor: "#0099CC",
+    width: "100%",
+  };
 
   return (
     <>
       <Grid>
         <Paper elevation={20} className="paperStyle">
-          <Grid align='center'>
+          <Grid align="center">
             <Avatar style={avatarStyle}>
-              <PersonAddIcon/>
+              {" "}
+              <PersonAddIcon />{" "}
             </Avatar>
             <h2 className="headerStyle">Creating a driver</h2>
-            <Typography variant = 'caption'>Fill in all the fields below</Typography>
-            </Grid>
-            <form>
-              <TextField fullWidth label='Name' placeholder='Enter your name' style={marginTop}/>
-              <TextField fullWidth label='Surname' placeholder='Enter your surname' style={marginTop}/>
-              <TextField fullWidth label='Email' placeholder='Enter your email'style={marginTop}/>
-              <TextField fullWidth label='Phone number' placeholder='Enter your phone number' style={marginTop}/>
-              <TextField fullWidth label='License number' placeholder='Enter your license number' style={marginTop}/>
-              <Button type='submit' variant='contained' color='primary' style={marginTop}>Add</Button>
-            </form>
+            <Typography variant="caption">
+              Fill in all the fields below
+            </Typography>
+          </Grid>
+          <form>
+            <TextField
+              fullWidth
+              label="Name"
+              id="Name"
+              placeholder="Enter your name"
+              style={marginTop}
+            />
+            <TextField
+              fullWidth
+              label="Surname"
+              id="Surname"
+              placeholder="Enter your surname"
+              style={marginTop}
+            />
+            <TextField
+              fullWidth
+              label="Email"
+              id="Email"
+              placeholder="Enter your email"
+              style={marginTop}
+            />
+            <TextField
+              fullWidth
+              label="Phone number"
+              id="PhoneNumber"
+              placeholder="Enter your phone number"
+              style={marginTop}
+            />
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label" style={bigMarginTop}>
+                Vehicle Type
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={vehicleType}
+                label="Vehicle type"
+                onChange={handleVehicleTypeChange}
+                style={bigMarginTop}
+              >
+                {vehicleTypes.map((type) => (
+                  <MenuItem value={type}>{type}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <TextField
+              fullWidth
+              label="License number"
+              id="VehicleType"
+              placeholder="Enter your license number"
+              style={marginTop}
+            />
+            <Button type="submit" variant="containeds" style={buttonStyle}>
+              Add
+            </Button>
+          </form>
         </Paper>
       </Grid>
     </>
   );
 };
-
 export default DriverComponent;
