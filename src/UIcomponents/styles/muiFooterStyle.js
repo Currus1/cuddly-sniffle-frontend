9c6999cdb99907eda4761f1a28a19640de3baed7
Klaidas Sinkevicius <clyde.sinkevicius@gmail.com>
2022-10-15 17:30:26 +0300

    NOT DONE. Started working on footer design

---
+++ src/UIcomponents/styles/muiFooterStyle.js
@@ -0,0 +1,43 @@
+export const footerStyle = {
+    backgroundColor: "#0099cc",
+    textAlign: "center",
+    padding: "20px",
+    bottom: "0",
+    height: "430px",
+    width: "100%"
+};
+export const footerLogoStyle = {
+    height: "400px",
+    fontSize: "70px",
+    fontWeight: "bold",
+    lineHeight: "75px"
+}
+export const footerLogoIConsStyle = {
+    width: "55%",
+    marginLeft: "30%",
+    marginRight: "auto",
+    fontSize: "70px",
+    fontWeight: "bold",
+    lineHeight: "75px"
+}
+export const footerLicensesStyle = {
+    height: "160px",
+    fontSize: "20px",
+    paddingLeft: "10px",
+    paddingTop: "5px",
+    textAlign: "left"
+}
+export const footerCurrusFeatures = {
+    height: "160px",
+    fontSize: "20px",
+    paddingLeft: "10px",
+    paddingTop: "5px",
+    textAlign: "left"
+}
+export const footerCompanyStyle = {
+    height: "160px",
+    fontSize: "20px",
+    paddingLeft: "10px",
+    paddingTop: "5px",
+    textAlign: "left"
+}
