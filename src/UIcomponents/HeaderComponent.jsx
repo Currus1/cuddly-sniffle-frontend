6af70acbcdf5d04db3913a2e81949c840d7cc485
Klaidas Sinkevicius <clyde.sinkevicius@gmail.com>
2022-10-16 20:21:17 +0300

    started work on the header

--- src/UIcomponents/HeaderComponent.jsx
+++ src/UIcomponents/HeaderComponent.jsx
@@ -1,44 +1,20 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
 import "../bootstrap.css";
-import Dropdown from "react-bootstrap/Dropdown";
+import {
+  headerStyle,
+  logoStyle
+} from "./styles/HeaderStyle.js"
 import "./styles/DropdownStyle.css";
-const HeaderComponent = () => (
-  <header>
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-      <div className="navbar-item" style={{ color: "white" }}>
-        CURRUS
-      </div>
-      <ul className="navbar-nav navbar-collapse justify-content-end">
-        <Dropdown>
-          <Dropdown.Toggle className="dropdown" id="dropdown">
-            Actions
-          </Dropdown.Toggle>
+import "./styles/HeaderStyle.css";
 
-          <Dropdown.Menu className="dropdown-menu">
-            <Dropdown.Item className="dropdown-item" href="/trip">
-              Ongoing trips
-            </Dropdown.Item>
-            <Dropdown.Item className="dropdown-item" href="/user">
-              Users
-            </Dropdown.Item>
-            <Dropdown.Item className="dropdown-item" href="/driver">
-              Drivers
-            </Dropdown.Item>
-            <Dropdown.Item className="dropdown-item" href="/trip">
-              All Trips
-            </Dropdown.Item>
-            <Dropdown.Item className="dropdown-item" href="/maps">
-              Map
-            </Dropdown.Item>
-            <Dropdown.Divider></Dropdown.Divider>
-            <Dropdown.Item className="dropdown-item" href="/planning">
-              Plan a trip
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </ul>
-    </nav>
-  </header>
+const HeaderComponent = () => (
+  <Grid  style={headerStyle}>
+    <Grid item xs={1} md={3} style={logoStyle}>
+      <img src="https://www.freeiconspng.com/thumbs/electricity-icon-png/electricity-icon--1.png" alt="" className="headerLogo" />
+      <p className="headerLogoText">CURRUS</p>
+    </Grid>
+  </Grid>
 );
 
 export default HeaderComponent;
---
+++ src/UIcomponents/styles/HeaderStyle.js
@@ -0,0 +1,8 @@
+export const headerStyle = {
+    backgroundColor: "#0099cc",
+    textAlign: "left",
+    height: "70px",
+}
+export const logoStyle = {
+    height: "100%",
+}
