75211dc86cdc43ee28cb6e401059aa037675b828
Klaidas Sinkevicius <clyde.sinkevicius@gmail.com>
2022-10-17 22:15:18 +0300

    deisgn updates

--- src/UIcomponents/HeaderComponent.jsx
+++ src/UIcomponents/HeaderComponent.jsx
@@ -3,16 +3,33 @@ import { Grid } from "@material-ui/core";
 import "../bootstrap.css";
 import {
   headerStyle,
-  logoStyle
-} from "./styles/HeaderStyle.js"
-import "./styles/DropdownStyle.css";
+  logoStyle,
+  rightColumnStyle,
+  textButtonStyle,
+  containedButtonStyle,
+  containedButtonRightStyle
+} from "./styles/HeaderStyle.js";
+import {
+  iconStyle,
+} from "./styles/muiStyle.js";
 import "./styles/HeaderStyle.css";
+import { Button } from '@material-ui/core';
+import Person2Icon from '@mui/icons-material/Person2';
 
 const HeaderComponent = () => (
-  <Grid  style={headerStyle}>
-    <Grid item xs={1} md={3} style={logoStyle}>
-      <img src="https://www.freeiconspng.com/thumbs/electricity-icon-png/electricity-icon--1.png" alt="" className="headerLogo" />
+  <Grid container style={headerStyle}>
+    <Grid item xs={12} md={6} style={logoStyle}>
+      <img
+        src="https://www.freeiconspng.com/thumbs/electricity-icon-png/electricity-icon--1.png"
+        alt=""
+        className="headerLogo"
+      />
+       <Button variant="contained" style={containedButtonRightStyle}>Plan A Trip!</Button>
       <p className="headerLogoText">CURRUS</p>
+    </Grid>
+    <Grid item xs={12} md={6} style={rightColumnStyle}>
+      <Button variant="contained" style={containedButtonStyle}>Become a Driver</Button>
+      <Button variant="text" style={textButtonStyle}><Person2Icon style={iconStyle}></Person2Icon></Button>
     </Grid>
   </Grid>
 );
--- src/UIcomponents/styles/HeaderStyle.js
+++ src/UIcomponents/styles/HeaderStyle.js
@@ -2,7 +2,31 @@ export const headerStyle = {
     backgroundColor: "#0099cc",
     textAlign: "left",
     height: "70px",
+    width: "100%"
 }
 export const logoStyle = {
+    //backgroundColor: "red",
     height: "100%",
+}
+export const rightColumnStyle = {
+    textAlign: "right"
+}
+export const textButtonStyle = {
+    marginRight: "5px",
+    marginTop: "8px",
+    color: "white"
+}
+export const containedButtonStyle = {
+    backgroundColor: "white",
+    marginTop: "9px",
+    marginRight: "10px",
+    fontWeight: "bold"
+}
+export const containedButtonRightStyle = {
+    backgroundColor: "white",
+    //color: "white",
+    //border: "2px solid",
+    marginLeft: "130px",
+    fontWeight: "bold",
+    width: "180px"
 }
