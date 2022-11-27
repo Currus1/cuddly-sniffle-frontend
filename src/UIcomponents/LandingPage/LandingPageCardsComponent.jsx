47fcff2a7235f929fd4582ad5889fbbb0f541b89
Klaidas Sinkevicius <clyde.sinkevicius@gmail.com>
2022-11-27 13:58:58 +0200

    remove old cards js file

--- src/UIcomponents/LandingPage/LandingPageCardsComponent.jsx
+++ src/UIcomponents/LandingPage/LandingPageCardsComponent.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import CardComponent from "./CardComponent";
 import CardContent from "../../Static/CardContent";
 import useWindowPosition from "../../CustomHooks/useWindowPosition";
-import { area } from "./Styles/HomePageCardsStyle.js";
+import style from "./Styles/HomePageCardsStyle.module.css";
 
 const HomePageCardsComponent = () => {
   const checked = useWindowPosition("header");
   return (
-    <div id="home-page-cards-component" style={area}>
+    <div id="home-page-cards-component" className={style.area}>
       <CardComponent cardContent={CardContent[0]} checked={checked} />
       <CardComponent cardContent={CardContent[1]} checked={checked} />
       <CardComponent cardContent={CardContent[2]} checked={checked} />
