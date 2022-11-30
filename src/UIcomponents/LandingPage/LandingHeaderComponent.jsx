ed501c07ca4d251a8ad45250e84dae5bb7cfdbc5
Klaidas Sinkevicius <clyde.sinkevicius@gmail.com>
2022-11-30 22:27:23 +0200

    add button navigation

--- src/UIcomponents/LandingPage/LandingHeaderComponent.jsx
+++ src/UIcomponents/LandingPage/LandingHeaderComponent.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { AppBar, IconButton } from "@material-ui/core";
 import SortIcon from "@material-ui/icons/Sort";
 import Toolbar from "@mui/material/Toolbar";
@@ -10,21 +11,25 @@ import styles from "./Styles/PageStyle.module.css";
 import headerStyles from "./Styles/LandingHeaderStyle.module.css";
 import currus from "../../Images/logo/currus_long_1.png";
 import importedHeaderStyles from "../BaseHeader/Styles/HeaderStyle.module.css";
+import { Navigate } from "react-router-dom";
 
 const HeaderComponent = () => {
   const [checked, setChecked] = useState(false);
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     setChecked(true);
   }, []);
+
   return (
     <div className={styles.page_container} id="header">
       <AppBar className={headerStyles.appBar}>
         <Toolbar className={importedHeaderStyles.header_bgcolor}>
           <img src={currus} className={importedHeaderStyles.logo}></img>
           <div className={headerStyles.buttonDivs}>
-            <button className={importedHeaderStyles.loginButton}>Log In</button>
-            <button className={importedHeaderStyles.registerButton}>Register</button>
+            <button className={importedHeaderStyles.loginButton} onClick={() => navigate("/login")}>Log In</button>
+            <button className={importedHeaderStyles.registerButton} onClick={() => navigate("/register")}>Register</button>
           </div>
         </Toolbar>
       </AppBar>
