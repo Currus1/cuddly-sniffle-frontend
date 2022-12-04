10b9062e44c08ad4a4d0b093662560b49f8c1af4
Klaidas Sinkevicius <clyde.sinkevicius@gmail.com>
2022-12-04 20:57:47 +0200

    fix some bugs

--- src/UIcomponents/LandingPage/LandingAnnouncementBar.jsx
+++ src/UIcomponents/LandingPage/LandingAnnouncementBar.jsx
@@ -6,7 +6,7 @@ const LandingAnnouncementBar = () => {
     <div className={styles.announcement_bar_container}>
       <div className={announcementStyle.container}>
         <div className={announcementStyle.announcement_text}>
-          <p>
+          <p className={announcementStyle.announcement}>
             🚀 Think of some kind of a text here 🚀
           </p>
         </div>
