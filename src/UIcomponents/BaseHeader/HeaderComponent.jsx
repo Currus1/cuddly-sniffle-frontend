834e7644e4b8371eb6f268339a2d7e8fa0fea54b
Klaidas Sinkevicius <clyde.sinkevicius@gmail.com>
2022-12-13 19:58:32 +0200

    header

--- src/UIcomponents/BaseHeader/HeaderComponent.jsx
+++ src/UIcomponents/BaseHeader/HeaderComponent.jsx
@@ -1,30 +1,20 @@
 import React, { useState } from "react";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import DrawerComponent from "./DrawerComponent";
 import styles from "./Styles/HeaderStyle.module.css";
-import currus from "../../Images/logo/currus_long_1.png";
+import currus from "../../Images/logo/currus_logo_2.png";
+
 
 const HeaderComponent = () => {
-  const [open, setOpen] = useState(false);
-
-  const ToggleDrawer = () => {
-    setOpen(!open);
-  };
-
   return (
     <Box>
-      <AppBar position="static">
-        <Toolbar className={styles.header_bgcolor}>
-          <DrawerComponent></DrawerComponent>
+      <div>
+        <Toolbar className={styles.header_container}>
+          <DrawerComponent />
           <img src={currus} className={styles.logo}></img>
-          <div className={styles.buttonDivs}>
-            <button className={styles.loginButton}>Log In</button>
-            <button className={styles.registerButton}>Register</button>
-          </div>
         </Toolbar>
-      </AppBar>
+      </div>
     </Box>
   );
 };
