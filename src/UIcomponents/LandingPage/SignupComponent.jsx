9afcdab3ce785a1264249377696776f6d98c0db6
Tedas <tedodarbo@gmail.com>
2023-03-31 23:55:18 +0300

    Landing

--- src/UIcomponents/LandingPage/SignupComponent.jsx
+++ src/UIcomponents/LandingPage/SignupComponent.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import styles from "./Styles/PageStyle.module.css";
 import TrendingUpIcon from "@mui/icons-material/TrendingUp";
 import AttributeStyle from "./Styles/AttributeStyle.module.css";
+import { useNavigate } from "react-router-dom";
 
 const HomePageCardsComponent = () => {
+  const navigate = useNavigate();
   return (
     <div className={styles.half_page_container}>
       <div>
         <h1 className={AttributeStyle.header_text}>Ready to carpool?</h1>
-        <a href="/register" className={AttributeStyle.button}>
+        <button
+          onClick={() => navigate("/register")}
+          className={AttributeStyle.button}
+        >
           Sign up now
-        </a>
+        </button>
         <br />
         <TrendingUpIcon className={AttributeStyle.arrowProp}></TrendingUpIcon>
       </div>
